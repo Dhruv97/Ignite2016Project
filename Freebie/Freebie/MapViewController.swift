916dc775d556fadaffcf5a14972c957152e1b28b
Vishal Vasan <vishvasan@gmail.com>
2016-11-16 00:37:12 -0500

    Added MapView

---
+++ Freebie/Freebie/MapViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MapViewController.swift
+//  Freebie
+//
+//  Created by Vishal Vasan on 11/16/16.
+//  Copyright © 2016 Ignite2016. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+
+    @IBOutlet weak var MapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MapView.delegate = self
+        MapView.userTrackingMode = MKUserTrackingMode.follow
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func locationAuth() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            MapView.showsUserLocation = true
+        }
+        else {
+            self.locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.locationAuth()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
