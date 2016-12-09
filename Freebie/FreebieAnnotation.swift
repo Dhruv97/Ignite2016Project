f875924ed62e9a75795f4a924997a2405b398422
Dhruv Upadhyay <1997dhruv@gmail.com>
2016-12-09 18:02:18 -0500

    tableview fix

---
+++ Freebie/FreebieAnnotation.swift
@@ -0,0 +1,16 @@
+//
+//  FreebieAnnotation.swift
+//  Freebie
+//
+//  Created by Dhruv Upadhyay on 12/9/16.
+//  Copyright © 2016 Ignite2016. All rights reserved.
+//
+
+import Foundation
+
+class FreebieAnnotation {
+    
+    
+    
+    
+}
